6f6f64acdd2ee3b61d11fea1c077fcf94f7f4047
vaibhav kohli <vaibhav.khl@gmail.com>
2015-05-27 02:02:50 +0530

    Initial commit includes
    rails api server and yeoman generated angular app

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Server::Application.config.secret_token = 'c27f897c432c28896007999272db286b785ade5ec316514607098c3d1c00c27d576654c6d5b6ccb31b8edb840ded995f36b354c28e0c8d65bec5bbd11a94dce4'
