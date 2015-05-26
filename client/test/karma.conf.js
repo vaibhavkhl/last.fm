b41a04d162c7983ee49fd6c582d5f9ddf9150edd
vaibhav kohli <vaibhav.khl@gmail.com>
2015-05-27 02:26:26 +0530

    grunt serve changes

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -22,6 +22,12 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/angular/angular.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+      'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
